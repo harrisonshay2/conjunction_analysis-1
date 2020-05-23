1eaad1708b530a38b7e9b46d8c5479a21db7308a
harrisonshay2 <harrisonshay2@gmail.com>
2020-05-23 14:00:34 -0400

    daily update

--- daily_update1.R
+++ daily_update1.R
@@ -3,7 +3,7 @@ library(fs)
 
 search.phrase <- 'Conjunction Reports'
 
-save.fldr <- 'C:\\Users\\Harri\\OneDrive\\Documents\\conjunction_analysis' # Set a root folder to save attachments into
+save.fldr <- 'C:\\Users\\Harri\\OneDrive\\Documents\\conjunction_analysis-1\\conj_data' # Set a root folder to save attachments into
 print(save.fldr)
 
 outlook_app <- COMCreate("Outlook.Application")
@@ -16,28 +16,15 @@ Sys.sleep(10) # Wait some time to allow search to complete
 
 results <- search[['Results']]
 
-#for(i in c(1:results[['Count']])){ # Loop through search results
- # attachments.obj <- results[[i]][['attachments']] # Gets the attachment object
 index = c(results[['Count']])
 
 
 attachments.obj <- results[[index]][['attachments']]
   
-  if(attachments.obj[['Count']] > 0){ # Check if there are attachments
-    attach.fldr <- file.path(save.fldr, path_sanitize(results[[i]][['Subject']])) # Set folder name for attachments based on email subject
-    
-    if(!dir.exists(attach.fldr)){
-      dir.create(attach.fldr) # Create the folder for the attachments if it doesn't exist
-    }
-    
-    
-    #for(a in c(1:attachments.obj[['Count']])){ # Loop through attachments
-      save.path <- file.path(attach.fldr, attachments.obj[[1]][['FileName']]) # Set the save path
+ 
+      save.path <- file.path(save.fldr, attachments.obj[[1]][['FileName']]) # Set the save path
       print(save.path)
       attachments.obj[[1]]$SaveAsFile(save.path) # Save the attachment
-    #}
     
-  }
-#}  
   
   
---
+++ notepadcmd.bat
@@ -0,0 +1,9 @@
+@echo off
+"C:\Program Files\R\R-4.0.0\bin\Rscript.exe" "C:\Users\Harri\OneDrive\Documents\conjunction_analysis-1\daily_update1.R" 
+timeout 30
+"C:\Program Files\R\R-4.0.0\bin\Rscript.exe" "C:\Users\Harri\OneDrive\Documents\conjunction_analysis-1\index.Rmd"
+
+"C:\Program Files\Git\bin\bash.exe" "C:\Users\Harri\OneDrive\Documents\conjunction_analysis-1\git_update.git"
+
+
+
