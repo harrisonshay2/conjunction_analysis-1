56c9310ed334969b461107a0801ef00b6450946f
harrisonshay2 <harrisonshay2@gmail.com>
2020-05-25 16:11:03 -0400

    daily update

--- notepadcmd.bat
+++ notepadcmd.bat
@@ -1,7 +1,7 @@
 @echo off
 "C:\Program Files\R\R-4.0.0\bin\Rscript.exe" "C:\Users\Harri\OneDrive\Documents\conjunction_analysis-1\daily_update1.R" 
 timeout 30
-"C:\Program Files\R\R-4.0.0\bin\Rscript.exe" -e "library('knitr'); Knit('C:/Users/Harri/OneDrive/Documents/conjunction_analysis-1/index.Rmd')
+"C:\Program Files\R\R-4.0.0\bin\Rscript.exe" -e "library('knitr'); knit('C:/Users/Harri/OneDrive/Documents/conjunction_analysis-1/index.Rmd')
 
 "C:\Program Files\Git\bin\bash.exe" "C:\Users\Harri\OneDrive\Documents\conjunction_analysis-1\git_update.git"
 
