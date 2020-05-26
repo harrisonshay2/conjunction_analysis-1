284abcc86335ea50497bcd876659db7e9ac587e5
harrisonshay2 <harrisonshay2@gmail.com>
2020-05-26 11:34:25 -0400

    daily update

--- notepadcmd.bat
+++ notepadcmd.bat
@@ -1,7 +1,6 @@
 @echo off
 "C:\Program Files\R\R-4.0.0\bin\Rscript.exe" "C:\Users\Harri\OneDrive\Documents\conjunction_analysis-1\daily_update1.R" 
 "C:\Program Files\R\R-4.0.0\bin\Rscript.exe" -e "library('knitr'); knitr('C:\Users\Harri\OneDrive\Documents\conjunction_analysis-1\index.Rmd')"
-"C:\Program Files\RStudio\bin\pandoc" -e "rmarkdown::render('index.md')"
 move index.html "C:\Users\Harri\OneDrive\Documents\conjunction_analysis-1\docs" 
 "C:\Program Files\Git\bin\bash.exe" "C:\Users\Harri\OneDrive\Documents\conjunction_analysis-1\git_update.git"
 
